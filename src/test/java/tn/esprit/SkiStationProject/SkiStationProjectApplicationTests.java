813aea87bb9afe7694e21202b31d9620b6fe5be3
raania ben salem <94994332+raniabns@users.noreply.github.com>
2024-04-26 12:13:56 +0100

    Update SkiStationProjectApplicationTests.java

--- src/test/java/tn/esprit/SkiStationProject/SkiStationProjectApplicationTests.java
+++ src/test/java/tn/esprit/SkiStationProject/SkiStationProjectApplicationTests.java
@@ -1,52 +1,13 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+package tn.esprit.SkiStationProject;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import tn.esprit.SkiStationProject.entities.Course;
-import tn.esprit.SkiStationProject.entities.enums.TypeCourse;
-import tn.esprit.SkiStationProject.entities.enums.Support;
-import tn.esprit.SkiStationProject.repositories.CourseRepository;
-import tn.esprit.SkiStationProject.services.CourseServicesImpl;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import static org.mockito.Mockito.*;
-public class SkiStationProjectApplicationTests {
-
-    @Mock
-    private SubscriptionRepository subscriptionRepository;
-
-    @InjectMocks
-    private SubscriptionServicesImpl subscriptionServices;
-
-    @Mock
-    private SkierRepository skierRepository;
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.openMocks(this);
-    }
+@SpringBootTest
+class SkiStationProjectApplicationTests {
 
     @Test
     void contextLoads() {
-        // Votre code de test pour le chargement du contexte Spring
-    }
-
-    @Test
-    public void testAddSubscription() {
-        Subscription subscription = new Subscription();
-        subscription.setTypeSub(TypeSubscription.ANNUAL);
-        subscription.setStartDate(LocalDate.of(2024, 1, 1));
-
-        when(subscriptionRepository.save(any(Subscription.class))).thenReturn(subscription);
-
-        Subscription savedSubscription = subscriptionServices.addSubscription(subscription);
-
-        assertEquals(TypeSubscription.ANNUAL, savedSubscription.getTypeSub());
-        assertEquals(LocalDate.of(2025, 1, 1), savedSubscription.getEndDate());
     }
 }
 
